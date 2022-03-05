43d71798dbc9bf1e0969d852cfd1ca0b3bc1fce0
Hyungsuk Choi <choihyungsuk@gmail.com>
2022-03-05 16:57:47 +0900

    complete modification: hello world

--- java_tutorials/src/org/opentutorials/javatutorials/eclipse/Helloworld.java
+++ java_tutorials/src/org/opentutorials/javatutorials/eclipse/Helloworld.java
@@ -1,9 +1,7 @@
 package org.opentutorials.javatutorials.eclipse;
 
 public class Helloworld {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
 	}
 
