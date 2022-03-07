22da4a1ab4ae02cfab400eb2567aaaf4fe475543
Hyungsuk Choi <choihyungsuk@gmail.com>
2022-03-07 21:16:53 +0900

    Using random variables in Java'

---
+++ java_tutorials/src/org/opentutorials/javatutorials/math/Random1.java
@@ -0,0 +1,20 @@
+package org.opentutorials.javatutorials.math;
+import java.util.Random;
+
+public class Random1 {
+
+	public static void main(String[] args) {
+		Random random = new Random();
+		
+		int x 		= random.nextInt(6) + 1;		// random variable between 1 and 6
+		double y 	= random.nextDouble(); 	// random variable between 0 and 1
+		boolean z 	= random.nextBoolean();	// true or false
+		
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+		
+
+	}
+
+}
