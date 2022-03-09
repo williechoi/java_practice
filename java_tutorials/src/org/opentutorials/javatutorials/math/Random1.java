43c46455aacabbe1d53dc903e58653bf87bc0f63
Hyungsuk Choi <choihyungsuk@gmail.com>
2022-03-09 22:52:51 +0900

    Random module (java)

--- java_tutorials/src/org/opentutorials/javatutorials/math/Random1.java
+++ java_tutorials/src/org/opentutorials/javatutorials/math/Random1.java
@@ -6,9 +6,9 @@ public class Random1 {
 	public static void main(String[] args) {
 		Random random = new Random();
 		
-		int x 		= random.nextInt(6) + 1;		// random variable between 1 and 6
-		double y 	= random.nextDouble(); 	// random variable between 0 and 1
-		boolean z 	= random.nextBoolean();	// true or false
+		int x = random.nextInt(6) + 1;		// random variable between 1 and 6
+		double y = random.nextDouble(); 	// random variable between 0 and 1
+		boolean z = random.nextBoolean();	// true or false
 		
 		System.out.println(x);
 		System.out.println(y);
