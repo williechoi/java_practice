b6f4ec32feffccd83aafdb80ff2ee886046457fb
Hyungsuk Choi <choihyungsuk@gmail.com>
2022-03-11 21:03:46 +0900

    Java: Basic if statement

---
+++ java_tutorials/src/org/opentutorials/javatutorials/ifstatement/Ifstatement.java
@@ -0,0 +1,18 @@
+package org.opentutorials.javatutorials.ifstatement;
+
+public class Ifstatement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age = 75;
+		
+		if(age >= 75) {
+			System.out.println("Ok Boomer!");
+		} else if(age >= 18) {
+			System.out.println("You are an adult!");
+		} else {
+			System.out.println("You are not an adult!");
+		}
+	}
+
+}
