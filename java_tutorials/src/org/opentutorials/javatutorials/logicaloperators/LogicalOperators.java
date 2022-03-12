11ce53c41b0fe2eb6555ae09bf53471eb3f55680
Hyungsuk Choi <choihyungsuk@gmail.com>
2022-03-12 23:08:50 +0900

    logical operator

---
+++ java_tutorials/src/org/opentutorials/javatutorials/logicaloperators/LogicalOperators.java
@@ -0,0 +1,18 @@
+package org.opentutorials.javatutorials.logicaloperators;
+
+public class LogicalOperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int temp = 25;
+		if (temp > 30) {
+			System.out.println("It is hot outside");
+		} else if (temp >= 20 && temp <= 30) {
+			System.out.println("It is warm outside");
+		} else {
+			System.out.println("It is cold outside");
+		}
+			
+	}
+
+}
