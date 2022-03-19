3d9255f0bd0688f1f49309148ce1fccb8baecfb3
Hyungsuk Choi <choihyungsuk@gmail.com>
2022-03-19 21:00:26 +0900

    2-D ArrayList

---
+++ java_tutorials/src/org/opentutorials/javatutorials/arraylist/TwoDArrayListMain.java
@@ -0,0 +1,39 @@
+package org.opentutorials.javatutorials.arraylist;
+import java.util.*;
+
+public class TwoDArrayListMain {
+
+	public static void main(String[] args) {
+		
+		ArrayList<ArrayList<String>> groceryList = new ArrayList();
+		
+		ArrayList<String> bakeryList = new ArrayList();
+		bakeryList.add("pasta");
+		bakeryList.add("garlic bread");
+		bakeryList.add("donuts");
+		
+		ArrayList<String> produceList = new ArrayList();	// produce = fruits + vegetables
+		produceList.add("tomatoes");
+		produceList.add("zucchini");
+		produceList.add("peppers");
+		
+		ArrayList<String> drinksList = new ArrayList();
+		drinksList.add("Coke");
+		drinksList.add("Pepsi");
+		
+		groceryList.add(bakeryList);
+		groceryList.add(produceList);
+		groceryList.add(drinksList);
+		
+		System.out.println(bakeryList);
+		System.out.println(bakeryList.get(0)); 
+		System.out.println(drinksList);
+		
+		System.out.println(groceryList);
+		System.out.println(groceryList.get(0));
+		System.out.println(groceryList.get(0).get(0));
+		
+		System.out.println(groceryList.get(2).get(1));
+	}
+
+}
