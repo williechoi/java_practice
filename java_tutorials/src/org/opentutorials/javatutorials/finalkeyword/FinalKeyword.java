14b4af42fe33967b36dc28345d423b0b0a8d9c60
Hyungsuk Choi <choihyungsuk@gmail.com>
2022-03-21 01:26:27 +0900

    Java: final keyword

---
+++ java_tutorials/src/org/opentutorials/javatutorials/finalkeyword/FinalKeyword.java
@@ -0,0 +1,12 @@
+package org.opentutorials.javatutorials.finalkeyword;
+
+public class FinalKeyword {
+
+	public static void main(String[] args) {
+		final double PI = 3.14159;
+		
+		System.out.println(PI);
+
+	}
+
+}
