9d621ad3f17268ef1002babc48e4f5191b9efbdc
Hyungsuk Choi <choihyungsuk@gmail.com>
2022-03-28 17:57:21 +0900

    Exception handling in Java

---
+++ java_tutorials/src/org/opentutorials/javatutorials/exceptionhandling/ExceptionHandling.java
@@ -0,0 +1,42 @@
+package org.opentutorials.javatutorials.exceptionhandling;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ExceptionHandling {
+
+	public static void main(String[] args) {
+
+		// exception = 	an event that occurs during the execution of a program that,
+		//				disrupts the normal flow of instructions
+		
+		Scanner scanner = new Scanner(System.in);
+		try {
+			
+			System.out.println("Enter a whole number to divide: ");
+			int x = scanner.nextInt();
+			
+			System.out.println("Enter a whole number to divide by: ");
+			int y = scanner.nextInt();
+			
+			int z = x/y;
+			
+			System.out.println("result: " + z);
+		} 
+		catch (ArithmeticException e) {
+			System.out.println("Cannot divide by zero! IDIOT!");
+		}
+		catch (InputMismatchException e) {
+			System.out.println("PLEASE ENTER A NUMBER OMFG!!!");
+		}
+		catch (Exception e) {
+			System.out.println("Something went wrong");
+		}
+		finally {
+			System.out.println("This will always print");
+			scanner.close();
+		}
+		
+
+	}
+
+}
