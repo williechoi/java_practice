5ab528bad1a069cf6a82c8e0455ab6c19475ee97
Hyungsuk Choi <choihyungsuk@gmail.com>
2022-05-29 20:30:29 +0900

    Opening a new window in Java GUI

---
+++ java_tutorials/src/org/opentutorials/javatutorials/newgui/NewGUI.java
@@ -0,0 +1,11 @@
+package org.opentutorials.javatutorials.newgui;
+
+public class NewGUI {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LaunchPage launchPage = new LaunchPage();
+	
+	}
+
+}
