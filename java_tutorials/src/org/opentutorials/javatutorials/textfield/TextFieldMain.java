6e6dbbde0a784868d4316225b288a81af9c63283
Hyungsuk Choi <choihyungsuk@gmail.com>
2022-06-04 23:07:22 +0900

    textField in Java GUI

---
+++ java_tutorials/src/org/opentutorials/javatutorials/textfield/TextFieldMain.java
@@ -0,0 +1,11 @@
+package org.opentutorials.javatutorials.textfield;
+
+public class TextFieldMain {
+
+	public static void main(String[] args) {
+		// JTextField = A GUI textbox component that can be used to add, set, or get text
+		
+		MyFrame frame = new MyFrame();
+	}
+
+}
