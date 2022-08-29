b67e48548717efe98aa99c29c39febde5236938a
Hyungsuk Choi <choihyungsuk@gmail.com>
2022-08-29 23:27:16 +0900

    book java amend

--- BookProject/src/Book.java
+++ BookProject/src/Book.java
@@ -2,8 +2,7 @@ public class Book {
     private String title;
     private String author;
 
-    public Book(String title, String author)
-    {
+    public Book(String title, String author) {
         this.title = title;
         this.author = author;
     }
