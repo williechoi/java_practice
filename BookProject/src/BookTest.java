920597c7c0ba13309191c9b6fb45062408154445
Hyungsuk Choi <choihyungsuk@gmail.com>
2022-08-31 23:53:09 +0900

    add more examples

--- BookProject/src/BookTest.java
+++ BookProject/src/BookTest.java
@@ -1,8 +1,12 @@
 public class BookTest {
     public static void main(String[] args) {
-        Book book = new Book("데미안", "헤르만 헤세");
+        Book book1 = new Book("데미안", "헤르만 헤세");
+        Book book2 = new Book("홍길동전", "허균");
+        Book book3 = new Book("보바리부인", "장 칼뱅");
 
-        System.out.println(book);
+        System.out.println(book1);
+        System.out.println(book2);
+        System.out.println(book3);
 
         String str = new String("test");
         System.out.println(str);
