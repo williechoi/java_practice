f4eb8f741cba96c05c7ace7f04e59fd759f756e1
Hyungsuk Choi <choihyungsuk@gmail.com>
2022-09-11 18:24:51 +0900

    ioc and di practice

---
+++ ioc/src/com/company/ioc/Encoder.java
@@ -0,0 +1,15 @@
+package com.company.ioc;
+
+public class Encoder {
+    private IEncoder iEncoder;
+
+    public Encoder(IEncoder iEncoder) {
+//        this.iEncoder = new Base64Encoder();
+        this.iEncoder = iEncoder;
+    }
+
+
+    public String encode(String message) {
+        return iEncoder.encode(message);
+    }
+}
---
+++ spring-ioc/src/main/java/com/example/springioc/Encoder.java
@@ -0,0 +1,21 @@
+package com.example.springioc;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Encoder {
+    private IEncoder iEncoder;
+
+    public Encoder(@Qualifier("urlEncoder") IEncoder iEncoder) {
+        this.iEncoder = iEncoder;
+    }
+
+    public String encode(String message) {
+        return iEncoder.encode(message);
+    }
+
+    public void setIEncoder(IEncoder iEncoder) {
+        this.iEncoder = iEncoder;
+    }
+}
